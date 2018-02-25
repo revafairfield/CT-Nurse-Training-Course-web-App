044d4bd6c08d34b8c2467564b418ed8988f04b5d
revafairfield <32856109+revafairfield@users.noreply.github.com>
2018-02-25 18:05:16 -0500

    Added welcome.php landing file after registration

---
+++ php/welcome.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Connecticut Nurses Training Application</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular-route.js"></script>
+
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/custom.css">
+
+</head>
+
+<body>
+
+<div class="container-fluid login-container">
+  <div class="row-fluid">
+      <img class="logo center-block" src="../images/ct-assoc-logo.png"/>
+      <h1 class="text-center">Nursing Online Training Application</h1>
+  </div>
+
+  <div class="row-fluid">
+  <div class="gray-bkgd container-padding border border-radius">
+
+    <b><font color = "blue" size = "3">Registration Successful</font></b>
+
+  </div>
+  </div>
+
+
+<div class="row-fluid padding-top">
+  <div class="container-padding border border-radius">
+    <p>Click here to?&nbsp;<a href="login.php">Login &rarr;</a></p>
+  </div>
+</div>
+
+
+
+</div>
+
+
+</body>
+
+</html
